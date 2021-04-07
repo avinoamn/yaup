6296f20068d57d2f454523820b7149b4666fc5d2
Avinoam Naamat <avi058532@gmail.com>
2021-04-07 22:13:37 +0300

    Fix docunmentation

--- debounce.js
+++ debounce.js
@@ -2,19 +2,19 @@ import uuid from './uuid.js';
 import { debounceDefaultOptions } from './defaultOptions.js';
 
 /**
- * Creates a debounced function that delays invoking `func` until after `wait`
+ * Creates a debounced function that delays invoking `callback` until after `wait`
  * milliseconds have elapsed. The debounced function comes with a `cancelAll` and 
- * `cancelLatest` methods to cancel delayed `func` invocations, and a `invokeAll`
+ * `cancelLatest` methods to cancel delayed `callback` invocations, and a `invokeAll`
  * and `invokeLatest` methods to immediately invoke them. Provide `options` to indicate
- * whether only the latest `func` should be invoked.
+ * whether only the latest `callback` should be invoked.
  *
  * @since 0.1.0
  * @category Function
- * @param {Function} func The function to debounce.
+ * @param {Function} callback The function to debounce.
  * @param {number} [wait=0] The number of milliseconds to delay.
  * @param {Object} [options={}] The options object.
  * @param {boolean} [options.applyOnlyLatest=false]
- *  Specify invoking only the latest debounced function called.
+ * Specify invoking only the latest debounced function called.
  * @returns {Function} Returns the new debounced function.
  */
 
